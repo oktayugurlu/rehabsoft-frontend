0bf09b92ce25ef481b0af79b241961a3f6f54b37
Oktay UGURLU <56557510+oktayugurlu@users.noreply.github.com>
2020-11-16 03:43:37 +0300

    Orthesis eklendi 7 tamamlandi

--- src/app/pages/user/necessaryforms/generalevaluationform/general-evaluation-form.component.ts
+++ src/app/pages/user/necessaryforms/generalevaluationform/general-evaluation-form.component.ts
@@ -49,6 +49,317 @@ export class GeneralEvaluationFormComponent implements OnInit {
     displayFormat:"shortdate",
   }
 
+  // Orthesis checkbox options
+  isOrthesisMap = [
+    {name: 'Tabanlık', value: false},
+    {name: 'Topuk Kapı', value: false},
+    {name: 'Ayak bileği hizasında ortez (supra-malleoler)',value:  false},
+    {name: 'Sabit Ayak-ayak bileği ortezi (AFO)', value: false},
+    {name: 'Eklemli Ayak-ayak bileği ortezi (eklemli AFO)', value: false},
+    {name: 'Eklemli Ayak-ayak bileği ortezi (eklemli AFO)', value: false},
+    {name: 'Dinamik ayak ayak bileği ortezi (DAFO)', value: false},
+    {name: 'Bacaklar için gece splinti', value: false},
+    {name: 'Bacaklar için gece splinti',value:  false},
+    {name: 'İmmobilizer', value: false},
+    {name: 'Kalça ateli', value: false},
+    {name: 'Gövde korsesi', value: false},
+    {name: 'Dirsek splinti', value: false},
+    {name: 'Baş parmak ortezi', value: false},
+  ];
+  orthesisMap = new Map([
+    ['Tabanlık', new Map([
+      ['left', false],
+      ['right', false]
+    ])],
+    ['Topuk Kapı', new Map([
+      ['left', false],
+      ['right', false]
+    ])],
+    ['Ayak bileği hizasında ortez (supra-malleoler)', new Map([
+      ['left', false],
+      ['right', false]
+    ])],
+    ['Sabit Ayak-ayak bileği ortezi (AFO)', new Map([
+      ['left', false],
+      ['right', false]
+    ])],
+    ['Eklemli Ayak-ayak bileği ortezi (eklemli AFO)', new Map([
+      ['left', false],
+      ['right', false]
+    ])],
+    ['Dinamik ayak ayak bileği ortezi (DAFO)', new Map([
+      ['left', false],
+      ['right', false]
+    ])],
+    ['Bacaklar için gece splinti', new Map([
+      ['left', false],
+      ['right', false]
+    ])],
+    ['İmmobilizer', new Map([
+      ['left', false],
+      ['right', false]
+    ])],
+    ['Kalça ateli', new Map([
+      ['left', false],
+      ['right', false]
+    ])],
+    ['Gövde korsesi', new Map([
+      ['left', false],
+      ['right', false]
+    ])],
+    ['Dirsek splinti', new Map([
+      ['left', false],
+      ['right', false]
+    ])],
+    ['Baş parmak ortezi', new Map([
+      ['left', false],
+      ['right', false]
+    ])]
+  ]);
+
+  orthesisTabanlikCheckBoxOptions = {
+    value: null,
+    onValueChanged: (e) => {
+      this.isOrthesisMap[0].value = e.component.option("value");
+    }
+  }
+  leftTabanlikCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Tabanlık', this.orthesisMap.get('Tabanlık').set('left',event.value));
+    },
+    text:"Sol taban"
+  }
+  rightTabanlikOrthesisCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Tabanlık', this.orthesisMap.get('Tabanlık').set('right',event.value));
+    },
+    text:"Sağ taban"
+  }
+  /////
+  orthesisTopukKapiCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.isOrthesisMap[1].value = event.component.option("value");
+    }
+  }
+  leftOrthesisTopukKapiCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Topuk Kapı', this.orthesisMap.get('Topuk Kapı').set('left',event.value));
+    },
+    text:"Sol topuk"
+  }
+  rightOrthesisTopukKapiCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Topuk Kapı', this.orthesisMap.get('Topuk Kapı').set('right',event.value));
+    },
+    text:"Sağ topuk"
+  }
+  /////
+  /////
+  orthesisAyakBilegiCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.isOrthesisMap[2].value = event.component.option("value");
+    },
+    width:22
+  }
+  leftOrthesisAyakBilegiCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Ayak bileği hizasında ortez (supra-malleoler)', this.orthesisMap.get('Ayak bileği hizasında ortez (supra-malleoler)').set('left',event.value));
+    },
+    text:"Sol ayak bileği"
+  }
+  rightOrthesisAyakBilegiCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Ayak bileği hizasında ortez (supra-malleoler)', this.orthesisMap.get('Ayak bileği hizasında ortez (supra-malleoler)').set('right',event.value));
+    },
+    text:"Sağ ayak bileği"
+  }/////
+  orthesisSabitAyakCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.isOrthesisMap[3].value = event.component.option("value");
+    }
+  }
+  leftOrthesisSabitAyakCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Sabit Ayak-ayak bileği ortezi (AFO)', this.orthesisMap.get('Sabit Ayak-ayak bileği ortezi (AFO)').set('left',event.value));
+    },
+    text:"Sol Ayak"
+  }
+  rightOrthesisSabitAyakCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Sabit Ayak-ayak bileği ortezi (AFO)', this.orthesisMap.get('Sabit Ayak-ayak bileği ortezi (AFO)').set('right',event.value));
+    },
+    text:"Sağ Ayak"
+  }/////
+  orthesisEklemliAyakCheckBoxOptions = {
+    onValueChanged: (event)=>{
+      this.isOrthesisMap[4].value = event.component.option("value");
+    }
+  }
+  leftOrthesisEklemliAyakCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Eklemli Ayak-ayak bileği ortezi (eklemli AFO)', this.orthesisMap.get('Eklemli Ayak-ayak bileği ortezi (eklemli AFO)').set('left',event.value));
+    },
+    text:"Sol Ayak"
+  }
+  rightOrthesisEklemliAyakCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Eklemli Ayak-ayak bileği ortezi (eklemli AFO)', this.orthesisMap.get('Eklemli Ayak-ayak bileği ortezi (eklemli AFO)').set('right',event.value));
+    },
+    text:"Sağ Ayak"
+  }/////
+  orthesisDinamikAyakCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.isOrthesisMap[5].value = event.component.option("value");
+    }
+  }
+  leftOrthesisDinamikAyakCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Dinamik ayak ayak bileği ortezi (DAFO)', this.orthesisMap.get('Dinamik ayak ayak bileği ortezi (DAFO)').set('left',event.value));
+    },
+    text:"Sol Ayak"
+  }
+  rightOrthesisDinamikAyakCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Dinamik ayak ayak bileği ortezi (DAFO)', this.orthesisMap.get('Dinamik ayak ayak bileği ortezi (DAFO)').set('right',event.value));
+    },
+    text:"Sağ Ayak"
+  }/////
+  orthesisBacakGeceCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.isOrthesisMap[6].value = event.component.option("value");
+    }
+  }
+  leftOrthesisBacakGeceCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Bacaklar için gece splinti', this.orthesisMap.get('Bacaklar için gece splinti').set('left',event.value));
+    },
+    text:"Sol Bacak"
+  }
+  rightOrthesisBacakGeceCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Bacaklar için gece splinti', this.orthesisMap.get('Bacaklar için gece splinti').set('right',event.value));
+    },
+    text:"Sağ Bacak"
+  }/////
+  orthesisImmobilizerCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.isOrthesisMap[7].value = event.component.option("value");
+    }
+  }
+  leftOrthesisImmobilizerCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('İmmobilizer', this.orthesisMap.get('İmmobilizer').set('left',event.value));
+    },
+    text:"Sol Omuz"
+  }
+  rightOrthesisImmobilizerCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('İmmobilizer', this.orthesisMap.get('İmmobilizer').set('right',event.value));
+    },
+    text:"Sağ Omuz"
+  }
+/////
+  orthesisKalcaAteliCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.isOrthesisMap[8].value = event.component.option("value");
+    }
+  }
+  // leftOrthesisKalcaAteliCheckBoxOptions = {
+  //   value: null,
+  //   onValueChanged: (event)=>{
+  //     this.orthesisMap.set('Kalça ateli', this.orthesisMap.get('Kalça ateli').set('left',event.value));
+  //   },
+  //   text:"Sağ bacak"
+  // }
+  // rightOrthesisKalcaAteliCheckBoxOptions = {
+  //   value: null,
+  //   onValueChanged: (event)=>{
+  //     this.orthesisMap.set('Kalça ateli', this.orthesisMap.get('Kalça ateli').set('right',event.value));
+  //   }
+  // }/////
+  orthesisGovdeKorsesiCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.isOrthesisMap[9].value = event.component.option("value");
+    }
+  }
+  // leftOrthesisGovdeKorsesiCheckBoxOptions = {
+  //   value: null,
+  //   onValueChanged: (event)=>{
+  //     this.orthesisMap.set('Gövde korsesi', this.orthesisMap.get('Gövde korsesi').set('left',event.value));
+  //   }
+  // }
+  // rightOrthesisGovdeKorsesiCheckBoxOptions = {
+  //   value: null,
+  //   onValueChanged: (event)=>{
+  //     this.orthesisMap.set('Gövde korsesi', this.orthesisMap.get('Gövde korsesi').set('right',event.value));
+  //   }
+  // }/////
+  orthesisDirsekSiplintiCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.isOrthesisMap[10].value = event.component.option("value");
+    }
+  }
+  leftOrthesisDirsekSiplintiCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Dirsek splinti', this.orthesisMap.get('Dirsek splinti').set('left',event.value));
+    },
+    text:"Sağ Dirsek"
+  }
+  rightOrthesisDirsekSiplintiCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Dirsek splinti', this.orthesisMap.get('Dirsek splinti').set('right',event.value));
+    },
+    text:"Sol Dirsek"
+  }/////
+  orthesisBasParmakCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.isOrthesisMap[11].value = event.component.option("value");
+    }
+  }
+  leftOrthesisBasParmakCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Baş parmak ortezi', this.orthesisMap.get('Baş parmak ortezi').set('left',event.value));
+    },
+    text:"Sol Parmak"
+  }
+  rightOrthesisBasParmakCheckBoxOptions = {
+    value: null,
+    onValueChanged: (event)=>{
+      this.orthesisMap.set('Baş parmak ortezi', this.orthesisMap.get('Baş parmak ortezi').set('right',event.value));
+    },
+    text:"Sağ Parmak"
+  }
+
+
 
   // Forms
   generalEvaluationForm  = {
@@ -67,6 +378,10 @@ export class GeneralEvaluationFormComponent implements OnInit {
     isSleptHospitalForHyperbilirubinemia: true,
     isAfterBirthReasonCerebralPalsy: true,
     isBotoxTreatment: true,
+
+    // orthesis variables, should be deleted and reformatted before submit
+    isOrthesisMap: this.isOrthesisMap,
+    orthesisMap: this.orthesisMap,
 
     // 1. Demografik Bilgiler
     gender: null,
@@ -403,13 +718,46 @@ export class GeneralEvaluationFormComponent implements OnInit {
     name: 'Yumuşak doku - kas cerrahisi',
     value: 'Yumuşak doku - kas cerrahisi'
   },{
-    name: 'kemik cerrahisi',
-    value: 'kemik cerrahisi'
+    name: 'Kemik cerrahisi',
+    value: 'Kemik cerrahisi'
   },{
     name: 'Yumuşak doku-kas ve Kemik bir arada',
     value: 'Yumuşak doku-kas ve Kemik bir arada'
   }];
 
+  //*** Other Orthesis Variables ***//
+  otherOrthesisOptions: any[] = [];
+  addOtherOrthesisButtonOptions: any = {
+    icon: "add",
+    text: "Cerrahi Ekle",
+    onClick: () => {
+      this.generalEvaluationForm.otherOrthesisInfoCollection.push(new AppliedSurgery());
+      this.otherOrthesisOptions = this.getOtherOrthesisOptions(this.generalEvaluationForm.otherOrthesisInfoCollection);
+    }
+  };
+
+  //*** Used Medicine Variables ***//
+  usedMedicineOptions: any[] = [];
+  addUsedMedicineButtonOptions: any = {
+    icon: "add",
+    text: "Cerrahi Ekle",
+    onClick: () => {
+      this.generalEvaluationForm.usedMedicineCollection.push(new UsedMedicine());
+      this.usedMedicineOptions = this.getUsedMedicineOptions(this.generalEvaluationForm.usedMedicineCollection);
+    }
+  };
+
+
+  //*** Used Medicine Variables ***//
+  expectationsAboutProgramOptions: any[] = [];
+  addExpectationsAboutProgramButtonOptions: any = {
+    icon: "add",
+    text: "Ekle",
+    onClick: () => {
+      this.generalEvaluationForm.expectationsAboutProgramCollection.push(new ExpectationsAboutProgram());
+      this.expectationsAboutProgramOptions = this.getExpectationsAboutProgramOptions(this.generalEvaluationForm.expectationsAboutProgramCollection);
+    }
+  };
 
   constructor(private router: Router,
               private userService: UserService,
@@ -478,6 +826,7 @@ export class GeneralEvaluationFormComponent implements OnInit {
       {
         stylingMode: "text",
         icon: "trash",
+        text: "Sil",
         onClick: () => {
           this.generalEvaluationForm.appliedSurgeryCollection.splice(index, 1);
           this.appliedSurgeryOptions = this.getAppliedSurgeryOptions(this.generalEvaluationForm.appliedSurgeryCollection);
@@ -495,6 +844,87 @@ export class GeneralEvaluationFormComponent implements OnInit {
     console.log(event.customItem);
   }
   //******* Applied Surgery Event Handlers end *******//
+
+
+  //******* Other Orthesis Event Handlers start *******//
+  getOtherOrthesisOptions(appliedSurgery: any) {
+    let options = [];
+    for (let i = 0; i < appliedSurgery.length; i++){
+      options.push(this.generateNewOtherOrthesisOptions1(i));
+    }
+    return options;
+  }
+  generateNewOtherOrthesisOptions1(index: number) {
+    return [
+      { stylingMode: 'outlined',
+        placeholder: 'Ortezin tanımı',
+        maxLength:200
+      },
+      {
+        stylingMode: "text",
+        icon: "trash",
+        text: "Sil",
+        onClick: () => {
+          this.generalEvaluationForm.otherOrthesisInfoCollection.splice(index, 1);
+          this.otherOrthesisOptions = this.getOtherOrthesisOptions(this.generalEvaluationForm.otherOrthesisInfoCollection);
+        }
+      }
+    ];
+  }
+  //******* Other Orthesis Event Handlers end *******//
+
+
+  //******* Used Medicine Event Handlers start *******//
+  getUsedMedicineOptions(appliedSurgery: any) {
+    let options = [];
+    for (let i = 0; i < appliedSurgery.length; i++){
+      options.push(this.generateNewUsedMedicineOptions1(i));
+    }
+    return options;
+  }
+  generateNewUsedMedicineOptions1(index: number) {
+    return [
+      { stylingMode: 'outlined',
+        placeholder: 'İlaç İsmi',
+        maxLength:200
+      },
+      {
+        stylingMode: "text",
+        icon: "trash",
+        onClick: () => {
+          this.generalEvaluationForm.usedMedicineCollection.splice(index, 1);
+          this.usedMedicineOptions = this.getUsedMedicineOptions(this.generalEvaluationForm.usedMedicineCollection);
+        }
+      }
+    ];
+  }
+  //******* Used Medicine Event Handlers end *******//
+
+
+  //******* Expectations Event Handlers start *******//
+  getExpectationsAboutProgramOptions(appliedSurgery: any) {
+    let options = [];
+    for (let i = 0; i < appliedSurgery.length; i++){
+      options.push(this.generateNewUsedMedicineOptions1(i));
+    }
+    return options;
+  }
+  generateNewExpectationsAboutProgramOptions1(index: number) {
+    return [
+      { stylingMode: 'outlined',
+        placeholder: '', maxLength:1000, minHeight: 100
+      },
+      {
+        stylingMode: "text",
+        icon: "trash",
+        onClick: () => {
+          this.generalEvaluationForm.expectationsAboutProgramCollection.splice(index, 1);
+          this.expectationsAboutProgramOptions = this.getExpectationsAboutProgramOptions(this.generalEvaluationForm.expectationsAboutProgramCollection);
+        }
+      }
+    ];
+  }
+  //******* Expectations Event Handlers end *******//
 
   private fillPatientAddress = ()=>{
     let newPatient;
@@ -518,6 +948,9 @@ export class GeneralEvaluationFormComponent implements OnInit {
     console.log(event.value);
   }
   uploadAppliedsSuergeryEpicrysisImage = (event) => {
+    console.log(event.value);
+  }
+  uploadOtherOrthesisImage = (event) => {
     console.log(event.value);
   }
 
