f500e5a1f15e2b94441bfb6f06fc1474cdc88752
Oktay UGURLU <56557510+oktayugurlu@users.noreply.github.com>
2021-03-07 11:42:03 +0300

    rearrange after prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,8 +10,8 @@
 
 export const environment = {
   production: true,
-  API_BASE_PATH:"https://rehabsoft-backend.herokuapp.com/api",
-  API_BASE_PATH_WS:"wss://rehabsoft-backend.herokuapp.com/api"
+  API_BASE_PATH:"http://localhost:8080/api",
+  API_BASE_PATH_WS:"ws://localhost:8080/api"
 };
 
 /*
