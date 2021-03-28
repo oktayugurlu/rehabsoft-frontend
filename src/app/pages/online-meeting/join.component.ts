6366e281547b3505ded5b8132b44a3ccfe534ce2
Oktay UGURLU <56557510+oktayugurlu@users.noreply.github.com>
2021-03-29 02:59:30 +0300

    REH-17 heroku again

--- src/app/pages/online-meeting/join.component.ts
+++ src/app/pages/online-meeting/join.component.ts
@@ -96,7 +96,18 @@ export class JoinComponent implements OnInit, OnDestroy{
     const configuration = {
       iceServers: [{
         urls: 'stun:stun.l.google.com:19302'
-      }]
+      },
+        {
+          'urls': 'turn:192.158.29.39:3478?transport=udp',
+          'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+          'username': '28224511:1379330808'
+        },
+        {
+          'urls': 'turn:192.158.29.39:3478?transport=tcp',
+          'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+          'username': '28224511:1379330808'
+        }]
+
     };
 
     // Prepare peer connection object
