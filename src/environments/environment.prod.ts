ebda9857d2e37c90ce5bbdfb0b01e4d3944a177a
Oktay UGURLU <56557510+oktayugurlu@users.noreply.github.com>
2021-04-01 00:09:18 +0300

    REH-17 http yapildi backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_BASE_PATH:"https://rehabsoft-backend.herokuapp.com/api",
-  API_BASE_PATH_WS:"wss://rehabsoft-backend.herokuapp.com/api"
+  API_BASE_PATH:"http://rehabsoft-backend.herokuapp.com/api",
+  API_BASE_PATH_WS:"ws://rehabsoft-backend.herokuapp.com/api"
 };
