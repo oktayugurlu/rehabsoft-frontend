d48a1b823876e125cedaf116f095aaeb7f65f365
Ahmet Deniz GÜNER <ahmetdenizguner@gmail.com>
2021-04-14 21:30:44 +0300

    assign doctor to patient completed

--- src/app/models/patient.ts
+++ src/app/models/patient.ts
@@ -1,7 +1,9 @@
 import {Parent} from './parent';
+import {User} from './user';
 
 export class Patient {
   tcKimlikNo: string;
   address: string;
   parentCollection: Parent[];
+  user: User;
 }
--- src/app/shared/services/patient.service.ts
+++ src/app/shared/services/patient.service.ts
@@ -6,6 +6,9 @@ import { environment } from '../../../environments/environment';
 import { PatientDetails } from 'src/app/models/patientdetails';
 
 import {DoctorInfo} from 'src/app/models/doctor-info';
+import {Patient} from '../../models/patient';
+import {Doctor} from '../../models/doctor';
+import {AssignedForm} from '../../models/dynamicform/assignedform';
 
 @Injectable({ providedIn: 'root' })
 export class PatientService {
@@ -18,6 +21,10 @@ export class PatientService {
         return this.http.get<PatientDetails[]>(`${environment.API_BASE_PATH}/patient/all`);
       }
 
+    getAllPatientByDoctor(doctorUserID:string) {
+      return this.http.get<PatientDetails[]>(`${environment.API_BASE_PATH}/patient/getByDoctor/${doctorUserID}`);
+    }
+
     getPatientByTcKimlikNo(tcKimlikNo:string){
       return this.http.get<PatientDetails>(`${environment.API_BASE_PATH}/patient/${tcKimlikNo}`);
     }
@@ -26,5 +33,16 @@ export class PatientService {
       return this.http.get<DoctorInfo>(`${environment.API_BASE_PATH}/patient/doctorinfo`);
     }
 
+    getNewRegistredPatient() {
+      return this.http.get<Patient[]>(`${environment.API_BASE_PATH}/patient/getnewregistredpatient`);
+    }
+
+    getDoctors() {
+      return this.http.get<Doctor[]>(`${environment.API_BASE_PATH}/doctor/getall`);
+    }
+
+  assignDoctorToPatient(doctorUserID: string, tcKimlikNo: string){
+    return this.http.post<any>(`${environment.API_BASE_PATH}/patient/assigndoctor/${tcKimlikNo}`, doctorUserID);
+  }
 
 }
