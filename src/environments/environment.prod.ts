13604d7f817eb5f991b7da348f72f355f3a495f0
erengulum <b21627265@cs.hacettepe.edu.tr>
2021-05-06 19:07:55 +0300

    pwa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,15 @@
+export const environment = {
+  production: true,
+  API_BASE_PATH:"https://rehabsoft-backend.herokuapp.com/api",
+  API_BASE_PATH_WS:"wss://rehabsoft-backend.herokuapp.com/api"
+};
 
+
+
+/*
 export const environment = {
   production: true,
   API_BASE_PATH:"http://localhost:8080/api",
   API_BASE_PATH_WS:"http://localhost:8080/api"
 };
+*/
